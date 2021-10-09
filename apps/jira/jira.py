0e9362a4503f4505ab4c408f7426b479dcee7bac
Adam Edison <aedison@touchtown.com>
2021-10-09 16:42:42 -0400

    aedison-working: override line clone for JIRA

---
+++ apps/jira/jira.py
@@ -0,0 +1,17 @@
+from talon import Context, actions
+ctx = Context()
+ctx.matches = r"""
+os: mac
+app: chrome
+win.title: /(JIRA)|(Task Assignment)/
+"""
+
+@ctx.action_class('edit')
+class EditActions:
+    def line_clone():
+        actions.edit.select_line()
+        actions.edit.copy()
+        actions.key('end')
+        actions.sleep('100ms')
+        actions.key('enter')
+        actions.edit.paste()
